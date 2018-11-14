e1229829270c5d8722e75764fd4970d5a5b1e867
Timothy McClure <tmcclure5@bloomberg.net>
2018-11-14 08:23:50 -0500

    Add private method spec support

---
+++ packages/babel-plugin-proposal-private-methods/src/index.js
@@ -0,0 +1,23 @@
+import { declare } from "@babel/helper-plugin-utils";
+import pluginClassFeatures, {
+  enableFeature,
+  FEATURES,
+} from "@babel/plugin-class-features";
+
+export default declare((api, options) => {
+  api.assertVersion(7);
+
+  const { loose } = options;
+
+  return {
+    inherits: pluginClassFeatures,
+
+    manipulateOptions(opts, parserOpts) {
+      parserOpts.plugins.push("classPrivateMethods");
+    },
+
+    pre() {
+      enableFeature(this.file, FEATURES.privateMethods, loose);
+    },
+  };
+});
